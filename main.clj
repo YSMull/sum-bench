bc2f2d5eb80b9697bdc5b5c20a391bb8096d5d55
毛煜苏 <maoyusu@corp.netease.com>
2022-08-19 23:53:14 +0800

    optimize clj to 10ms

--- main.clj
+++ main.clj
@@ -1,12 +1,16 @@
 (ns json-parser-clj.main)
 
-
-(defn perform []
-  (reduce + (range 10000001)))
-
+; 加这个 type hint 性能提高一倍
+(defn ^long perform []
+  (loop [s 0
+         i 1]
+    (if (< i 10000001)
+      (recur (+ s i) (inc i))
+      s)))
 
 (defn -main []
   (do
+    (println (perform))
     (time (perform))
     (time (perform))
     (time (perform))
