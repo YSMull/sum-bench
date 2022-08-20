2862b226e08d7e00935a14576edfadb742c36c7d
毛煜苏 <maoyusu@corp.netease.com>
2022-08-20 14:09:41 +0800

    update scala

--- main.scala
+++ main.scala
@@ -1,19 +1,10 @@
 object Main {
 
-  def time[R](block: => R): R = {
-    val t0 = System.nanoTime()
-    val result = block
-    val t1 = System.nanoTime()
-    println("Elapsed time: " + (t1 - t0) / 1000000.0 + "ms")
-    result
-  }
-
   def perform(n: Int): Long = {
     var s = 0L
     for (i <- 1 to n) {
       s += i
     }
-    println(s)
     s
   }
 
@@ -21,9 +12,10 @@ object Main {
     val inputs = "10000000\n10000000\n10000000\n10000000\n10000000\n10000000\n10000000\n10000000\n10000000\n10000000\n10000000\n10000000"
     val numbers = inputs.split("\n").map(Integer.parseInt)
     for (n <- numbers) {
-      time {
-        perform(n)
-      }
+      val t0 = System.nanoTime()
+      val result = perform(n)
+      val t1 = System.nanoTime()
+      println(s"$result Elapsed time: " + (t1 - t0) / 1000000.0 + "ms")
     }
   }
 }
